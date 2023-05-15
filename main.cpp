a01f6cf8df525eaf414e741ffe734cc2b4f61e27
SalmaGamal06 <102250131+SalmaGamal06@users.noreply.github.com>
2023-05-15 17:10:25 +0300

    Update main.cpp (bst options)

--- main.cpp
+++ main.cpp
@@ -10,9 +10,9 @@ using namespace std;
 int main(){
 
     //The first menu
-    int choice=0;
+    int choice = 0, choice2;
     while(true){
-        cout<<"Welcome!Choose the data structure:\n"
+        cout<<"Welcome! Choose the data structure:\n"
               "1. BST\n"
               "2. AVL\n"
               "3. Min Heap\n"
@@ -20,13 +20,41 @@ int main(){
               "5. Exit Program"<<endl;
         cin>>choice;
         //BST Menu
-        if(choice==1) {
+        if (choice == 1) {
             cout << "Choose one of the following options:\n"
                     "1. Add student \n"
                     "2. Remove student\n"
                     "3. Search student\n"
                     "4. Print All (sorted by id)\n"
                     "5. Return to main menu" << endl;
+
+            //insert choice form menu 2
+            cin >> choice2;
+            BST bst;
+            //load info from file to BST
+            loadFromFile(bst);
+
+            while (true) {
+                if (choice2 == 1) {
+                    addStudent(bst);
+                } else if (choice2 == 2) {
+                    removeStudent(bst);
+                } else if (choice2 == 3) {
+                    searchStudent(bst);
+                } else if (choice2 == 4) {
+                    bst.print();
+                } else if (choice2 == 5) {
+                    break;
+                }
+                cout << "Choose one of the following options:\n"
+                        "1. Add student \n"
+                        "2. Remove student\n"
+                        "3. Search student\n"
+                        "4. Print All (sorted by id)\n"
+                        "5. Return to main menu" << endl;
+                cin >> choice2;
+            }
+
         }
         //AVL Menu
         if(choice==2) {
@@ -36,7 +64,7 @@ int main(){
                     "3. Search student\n"
                     "4. Print All (sorted by id)\n"
                     "5. Return to main menu" << endl;
-            int choice2;
+            
             cin >> choice2;
 
             AVL avl;
@@ -68,19 +96,19 @@ int main(){
             minHeap minheap;
             vector<Student> te;
             loadFromFileIntoTree(minheap);
-            int minHeapChoice;
+            
             cout << "Choose one of the following options:\n"
                     "1. Add student\n"
                     "2. Print All (sorted by gpa)\n"
                     "3. Return to the main menu"<<endl;
 
-            cin >> minHeapChoice;
+            cin >> choice2;
             while(true){
-                if (minHeapChoice == 1) {
+                if (choice2 == 1) {
                     minAddStudent(minheap);
-                } else if (minHeapChoice == 2) {
+                } else if (choice2 == 2) {
                     printAll(minheap);
-                } else if(minHeapChoice==3) {
+                } else if(choice2==3) {
                     break;
                 }
                 cout << "Choose one of the following options:\n"
@@ -88,32 +116,32 @@ int main(){
                          "2. Print All (sorted by gpa)\n"
                          "3. Return to the main menu"<<endl;
 
-                cin >> minHeapChoice;
+                cin >> choice2;
             }
 
         }
         //Maxheap choice
         if(choice==4) {
-            int maxHeapChoice;
+            
             maxHeap maxheap;
             Student student;
             cout << "Choose one of the following options:\n"
                     "1. Add student\n"
                     "2. Print All (sorted by gpa)\n"
                     "3. Return to the main menu"<<endl;
-            cin >> maxHeapChoice;
+            cin >> choice2;
             while(true){
-                if (maxHeapChoice == 1) {
+                if (choice2 == 1) {
                     maxAddStudent(maxheap);
-                } else if (maxHeapChoice == 2) {
+                } else if (choice2 == 2) {
                     printAll(maxheap);
-                } else if(maxHeapChoice == 3) {
+                } else if(choice2 == 3) {
                     break;
                 }cout << "Choose one of the following options:\n"
                          "1. Add student\n"
                          "2. Print All (sorted by gpa)\n"
                          "3. Return to the main menu"<<endl;
-                cin >> maxHeapChoice;
+                cin >> choice2;
 
             }
                     }
